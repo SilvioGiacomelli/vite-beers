175a938f3f05e5f72fb85b2b86a4a50920cd7b40
SilvioGiacomelli <156215274+SilvioGiacomelli@users.noreply.github.com>
2024-03-28 11:51:45 +0100

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script>
+import {store} from '../data/store'
+  export default {
+    data(){
+      return{
+        store,
+        nameToSearch:''
+      }
+    },
+    methods:{
+      
+    }
+  }
+</script>
+<template>
+  <div>
+
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<script>
+import Card from './partials/Card.vue'
+import {store} from '../data/store'
+  export default {
+    components:{
+      Card
+    },
+    data(){
+      return{
+        store
+      }
+    }
+  }
+</script>
+<template>
+  <div>
+
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
